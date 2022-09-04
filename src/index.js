edcb2c6befe232af98a141405cefbbf8988ed913
Shreya Vijayvargiya <arpit1993vijay@gmail.com>
2022-09-05 00:27:13 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './base.css';
+
+import './index.css';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//  serviceWorkerRegistration.register();
+
+// swDev();
+// reportWebVitals();
